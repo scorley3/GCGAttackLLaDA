9510b118e507cb89887ed7739abbf742c5a66ecf
zavaonah <rubenzneyroud@gmail.com>
2025-11-12 21:29:27 +0000

    Rename LLaDA

--- project.py
+++ project.py
@@ -6,6 +6,9 @@ from gcg_single import gcg_single_attack_loss
 from gcg_qwen import qwen_gcg_single_attack 
 from llada import get_llada_model
 from qwen import get_qwen_model
+from LLaDA_main import generate
+from LLaDA_main import get_log_likelihood
+
 
 prompts, targets = get_dataset()
 
