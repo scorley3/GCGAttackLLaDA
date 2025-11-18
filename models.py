46c367ce95f3264b77afc5e040e2cb55f31f45f0
zavaonah <rubenzneyroud@gmail.com>
2025-11-18 00:55:40 +0000

    attack 11/17

---
+++ models.py
@@ -0,0 +1,63 @@
+import torch
+from transformers import BitsAndBytesConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
+import gc
+
+def get_qwen_model():
+    model_name_qwen = "Qwen/Qwen2.5-3B-Instruct"
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model_qwen = AutoModelForCausalLM.from_pretrained(
+        model_name_qwen,
+        torch_dtype="auto",
+        device_map="auto"
+    )
+    model_qwen.to(device)
+    tokenizer = AutoTokenizer.from_pretrained(model_name_qwen)
+
+    return tokenizer, model_qwen
+
+# Quantization config for 8-bit to use with not A100
+# bnb_config = BitsAndBytesConfig(
+#     load_in_8bit=True,
+#     llm_int8_threshold=6.0  # optional, helps with stability
+# )
+def get_llada_model():
+    tokenizer = AutoTokenizer.from_pretrained('GSAI-ML/LLaDA-8B-Instruct', trust_remote_code=True)
+
+    model = AutoModel.from_pretrained(
+        'GSAI-ML/LLaDA-8B-Instruct',
+        #quantization_config=bnb_config,
+        device_map="auto",
+        trust_remote_code=True
+    )
+
+    return tokenizer, model
+
+
+def load_qwen(device="cuda"):
+    print("\n[Loading Qwen model...]")
+    tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-3B-Instruct")
+    model = AutoModelForCausalLM.from_pretrained(
+        "Qwen/Qwen2.5-3B-Instruct",
+        dtype="float16",
+        device_map=device,
+    )
+    return model, tokenizer
+
+
+def load_llada(device="cuda"):
+    print("\n[Loading LLADA model...]")
+    tokenizer = AutoTokenizer.from_pretrained("GSAI-ML/LLaDA-8B-Instruct", trust_remote_code=True)
+    model = AutoModel.from_pretrained(
+        "GSAI-ML/LLaDA-8B-Instruct",
+        dtype="float16",
+        device_map=device,
+        trust_remote_code=True
+    )
+    return model, tokenizer
+
+def unload_model(model):
+    print("[Unloading model from GPU to free VRAM...]")
+    del model
+    gc.collect()
+    torch.cuda.empty_cache()
